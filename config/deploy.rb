04131db325599f871dd651222127d9ddb792753c
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2025-03-25 08:00:29 +0100

    added capistrano3-puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,21 +59,19 @@ set :default_env, {
 set :puma_threads, [4, 16]
 set :puma_workers, 2
 
-set :puma_bind, "unix://#{shared_path}/sockets/puma.sock"
-set :puma_state, "#{shared_path}/pids/puma.state"
-set :puma_pid, "#{shared_path}/pids/puma.pid"
-set :puma_access_log, "#{shared_path}/log/puma.access.log"
-set :puma_error_log, "#{shared_path}/log/puma.error.log"
+set :puma_bind, "unix:///var/www/fast_track/shared/sockets/puma.sock"
+set :puma_state, "/var/www/fast_track/shared/pids/puma.state"
+set :puma_pid, "/var/www/fast_track/shared/pids/puma.pid"
+set :puma_access_log, "/var/www/fast_track/shared/log/puma.access.log"
+set :puma_error_log, "/var/www/fast_track/shared/log/puma.error.log"
 
 set :puma_preload_app, true
-set :puma_daemonize, false
-set :puma_prune_bundler, true
+# set :puma_prune_bundler, true
 
 set :puma_service_unit_name, "puma_#{fetch(:application)}"
-set :puma_enable_socket_service, true
 set :puma_systemctl_user, :system
-# set :puma_restart_command, 'bundle exec puma -C config/puma.rb'
-set :puma_restart_command, '/usr/local/bin/bundle exec /var/www/fast_track/shared/bundle/ruby/3.2.0/bin/puma -C config/puma.rb'
+set :puma_restart_command, 'bundle exec puma -C config/puma.rb'
+#set :puma_restart_command, '/usr/local/bin/bundle exec /var/www/fast_track/shared/bundle/ruby/3.2.0/bin/puma -C config/puma.rb'
 
 
 
