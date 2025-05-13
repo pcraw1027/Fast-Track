ce0840961ae4a38f6718de75b99cf3eea524d9aa
Nsikan Ikpoh <nsikanikpoh@gmail.com>
2025-05-13 08:32:37 +0100

    added my_scans endpoint and paginated results

--- app/services/paginated_result.rb
+++ app/services/paginated_result.rb
@@ -1,22 +1,29 @@
 class PaginatedResult
-  attr_reader :records, :limit, :page, :total_count
+  attr_reader :records, :per_page, :page, :total_count, :total_pages, :next_page, :prev_page
 
-  def initialize(records, limit, page, total_count)
+  def initialize(records, per_page, page, total_count)
     @records = records
-    @limit = limit
+    @per_page = per_page
     @page = page
     @total_count = total_count
+    @total_pages = compute_total_pages
+    @next_page = compute_next_page
+    @prev_page = compute_prev_page
   end
 
-  def total_pages
-    (total_count.to_f / limit).ceil
+
+  private
+
+
+  def compute_total_pages
+    (total_count.to_f / per_page).ceil
   end
 
-  def next_page
+  def compute_next_page
     page < total_pages ? page + 1 : nil
   end
 
-  def prev_page
+  def compute_prev_page
     page > 1 ? page - 1 : nil
   end
 end
